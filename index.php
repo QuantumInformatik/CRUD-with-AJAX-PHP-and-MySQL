8271ffb4ff006ed50acc3b2f95ab1e930c5dee70
Quantum <juanguillermoalarcon@gmail.com>
2017-12-30 00:27:08 -0500

    Tabulaciones x2

--- index.php
+++ index.php
@@ -17,45 +17,41 @@
 			<div class="jumbotron" style="background: #F76464 ">
 			<h1 style="color: white">Crud Ajax PHP MYSQL</h1>
 			  <p>...</p>
-			  
 			</div>
 
 			<div class="row">
 				<div class="col-md-6">	
-				<form id="finsertar" >
-					<h3>Formulario de Inserción</h3>
-						<table class="table table-hover ">
-							<tr>
-								<th>Producto</th>
-								<td><input autofocus type="text" name="producto" required="" class="form-control" placeholder="Ingrese nombre del producto"></td>
-							</tr>
-							<tr>
-								<th>Cantidad</th>
-								<td>
-									<input type="number" name="cantidad"  class="form-control" placeholder="Ingrese cantidad">
-								</td>
-							</tr>
-							<tr>
-								<th>Precio</th>
-								<td><input type="number" name="precio"  class="form-control" placeholder="Ingrese precio"></td>
-							</tr>
-							<tr>
-								<td> </td>
-								<td><input type="submit" name="insertar"  id="cagar" value="Insertar en la BD" class="btn btn-primary"></td>
-								<td><span id="insertar"></span></td> 
-								<td><span id="remove"></span></td>
-								<td><span id="modificar"></span></td>
-							</tr>
-				
-						</table>
-				</form >
-				<form id="fmod" >				
-				</form>
-
+					<form id="finsertar" >
+						<h3>Formulario de Inserción</h3>
+							<table class="table table-hover ">
+								<tr>
+									<th>Producto</th>
+									<td><input autofocus type="text" name="producto" required="" class="form-control" placeholder="Ingrese nombre del producto"></td>
+								</tr>
+								<tr>
+									<th>Cantidad</th>
+									<td>
+										<input type="number" name="cantidad"  class="form-control" placeholder="Ingrese cantidad">
+									</td>
+								</tr>
+								<tr>
+									<th>Precio</th>
+									<td><input type="number" name="precio"  class="form-control" placeholder="Ingrese precio"></td>
+								</tr>
+								<tr>
+									<td> </td>
+									<td><input type="submit" name="insertar"  id="cagar" value="Insertar en la BD" class="btn btn-primary"></td>
+									<td><span id="insertar"></span></td> 
+									<td><span id="remove"></span></td>
+									<td><span id="modificar"></span></td>
+								</tr>
+							</table>
+					</form >
+					<form id="fmod" >				
+					</form>
 				</div>
 
-				<div class="col-md-6" id="mostrar">
-				</div>
+				<div class="col-md-6" id="mostrar"></div>
 			</div>
 			<a class="btn btn-primary" href="php/truncate.php">Truncate</a>
 		</div><!-- End Container -->
